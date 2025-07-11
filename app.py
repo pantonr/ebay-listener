31b9428803c83d70735ab574043ea81f367f317b
pantonr <philip@p-ant.com>
2025-07-11 17:53:34 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+VERIFICATION_TOKEN = "your_secret_token"
+
+@app.route("/ebay-account-deletion", methods=["POST"])
+def account_deletion():
+    token = request.headers.get("X-Verification-Token")
+    if token != VERIFICATION_TOKEN:
+        abort(403)
+    print("Received deletion event:", request.json)
+    return "", 200
+
+if __name__ == "__main__":
+    app.run()
